018fa37ea42d7d594d8b51416fcb015b95d0ce62
sanjayyy04 <smchauhan2004@gmail.com>
2025-10-29 17:28:12 +0530

    secured api key

--- src/components/constant.js
+++ src/components/constant.js
@@ -1,4 +1,4 @@
 export const API_URL =
     "https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent";
 
-export const API_KEY = "AIzaSyDRnWWaqN5k19oWTwm54N5tPIZxBkj2NO0";
+export const API_KEY = import.meta.env.VITE_GEMINI_API_KEY;
